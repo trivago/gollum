82e4bda9a62bde6a502ebd0f388d6ceb7f53e23a
Arne Claus <arne.claus@trivago.com>
2015-02-25 17:01:25 +0100

    Cleanup + better memory profiling

--- flags.go
+++ flags.go
@@ -8,22 +8,20 @@ import (
 
 var (
 	helpPtr       = flag.Bool([]string{"h", "-help"}, false, "Print usage")
-	configFilePtr = flag.String([]string{"c", "-config"}, "", "Configuration file")
 	versionPtr    = flag.Bool([]string{"v", "-version"}, false, "Print version information and quit")
+	msgProfilePtr = flag.Bool([]string{"t", "-throughput"}, false, "Write msg/sec measurements to log")
 	numCPU        = flag.Int([]string{"n", "-numcpu"}, 0, "Number of CPUs to use")
+	configFilePtr = flag.String([]string{"c", "-config"}, "", "Configuration file")
 	cpuProfilePtr = flag.String([]string{"cp", "-cpuprofile"}, "", "Write cpu profiler results to a given file")
 	memProfilePtr = flag.String([]string{"mp", "-memprofile"}, "", "Write heap profile to a given file")
-	msgProfilePtr = flag.Bool([]string{"t", "-throughput"}, false, "Write msg/sec measurements to log")
 	pidFilePtr    = flag.String([]string{"p", "-pidfile"}, "", "Write the process id into a given file")
 )
 
 func init() {
 	flag.Usage = func() {
-		fmt.Fprint(os.Stdout, "Usage: gollum [OPTIONS]\n\nGollum - A n:m message multiplexer.\n\nOptions:\n")
-
+		fmt.Println("Usage: gollum [OPTIONS]\n\nGollum - A n:m message multiplexer.\n\nOptions:")
 		flag.CommandLine.SetOutput(os.Stdout)
-
 		flag.PrintDefaults()
-		fmt.Fprintf(os.Stdout, "\n")
+		fmt.Print("\n")
 	}
 }
--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"runtime"
 	"runtime/pprof"
 	"strconv"
+	"time"
 )
 
 const (
@@ -21,8 +22,23 @@ const (
 	gollumPatchVer = 0
 )
 
+func dumpMemoryProfile() {
+	if file, err := os.Create(*memProfilePtr); err != nil {
+		panic(err)
+	} else {
+		defer file.Close()
+		pprof.WriteHeapProfile(file)
+	}
+}
+
 func main() {
 	flag.Parse()
+
+	if *helpPtr || *configFilePtr == "" {
+		flag.Usage()
+		fmt.Println("Nothing to do. We must go.")
+		return
+	}
 
 	if *versionPtr {
 		fmt.Printf("Gollum v%d.%d.%d\n", gollumMajorVer, gollumMinorVer, gollumPatchVer)
@@ -35,40 +51,33 @@ func main() {
 		runtime.GOMAXPROCS(*numCPU)
 	}
 
-	if *helpPtr || *configFilePtr == "" {
-		flag.Usage()
-		fmt.Println("Nothing to do. We must go.")
-		return
-	}
-
-	if *cpuProfilePtr != "" {
-		file, err := os.Create(*cpuProfilePtr)
-		if err != nil {
-			panic(err)
-		}
-		pprof.StartCPUProfile(file)
-		defer func() {
-			pprof.StopCPUProfile()
-			file.Close()
-		}()
-	}
-
 	if *pidFilePtr != "" {
 		ioutil.WriteFile(*pidFilePtr, []byte(strconv.Itoa(os.Getpid())), 0644)
 	}
+
+	if *cpuProfilePtr != "" {
+		if file, err := os.Create(*cpuProfilePtr); err != nil {
+			panic(err)
+		} else {
+			defer file.Close()
+			pprof.StartCPUProfile(file)
+			defer pprof.StopCPUProfile()
+		}
+	}
+
+	if *memProfilePtr != "" {
+		ticker := time.NewTicker(time.Second)
+		defer ticker.Stop()
+		go func() {
+			for {
+				<-ticker.C
+				dumpMemoryProfile()
+			}
+		}()
+	}
+
 	// Start the gollum multiplexer
 
 	plex := newMultiplexer(*configFilePtr, *msgProfilePtr)
 	plex.run()
-
-	// Memory profiling
-
-	if *memProfilePtr != "" {
-		file, err := os.Create(*memProfilePtr)
-		if err != nil {
-			panic(err)
-		}
-		pprof.WriteHeapProfile(file)
-		file.Close()
-	}
 }
