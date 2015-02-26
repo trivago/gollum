bd1146715adaa34d1f503b48d914eeb0201434d5
Arne Claus <arne.claus@trivago.com>
2015-02-26 09:54:25 +0100

    Added Broadcast and Random distributors

---
+++ distributor/Broadcast.go
@@ -0,0 +1,33 @@
+package distributor
+
+import (
+	"github.com/trivago/gollum/shared"
+)
+
+// Broadcast distributor plugin
+// Configuration example
+//
+// - "distributor.Standard":
+//   Enable: true
+//   Stream: "*"
+//
+// This consumer does not define any options beside the standard ones.
+// Messages are send to all of the producers listening to the given stream.
+type Broadcast struct {
+}
+
+func init() {
+	shared.RuntimeType.Register(Broadcast{})
+}
+
+// Configure initializes this distributor with values from a plugin config.
+func (dist *Broadcast) Configure(conf shared.PluginConfig) error {
+	return nil
+}
+
+// Distribute sends the given message to all of the given producers
+func (dist *Broadcast) Distribute(message shared.Message, producers []shared.Producer, sendToInactive bool) {
+	for _, producer := range producers {
+		shared.SingleDistribute(producer, message, sendToInactive)
+	}
+}
---
+++ distributor/Random.go
@@ -0,0 +1,35 @@
+package distributor
+
+import (
+	"github.com/trivago/gollum/shared"
+	"math/rand"
+)
+
+// Random distributor plugin
+// Configuration example
+//
+// - "distributor.Random":
+//   Enable: true
+//   Stream: "*"
+//
+// This consumer does not define any options beside the standard ones.
+// Messages are send to a random producer in the set of the producers listening
+// to the given stream.
+type Random struct {
+}
+
+func init() {
+	shared.RuntimeType.Register(Random{})
+}
+
+// Configure initializes this distributor with values from a plugin config.
+func (dist *Random) Configure(conf shared.PluginConfig) error {
+	return nil
+}
+
+// Distribute sends the given message to one random producer in the set of
+// given producers.
+func (dist *Random) Distribute(message shared.Message, producers []shared.Producer, sendToInactive bool) {
+	index := rand.Intn(len(producers))
+	shared.SingleDistribute(producers[index], message, sendToInactive)
+}
