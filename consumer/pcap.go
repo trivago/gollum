28fc3a6b8c35b827a53f81795445c2a56a874f43
Arne Claus <arne.claus@trivago.com>
2015-05-22 11:35:14 +0200

    cleanup and config adjustments for testing environment

--- consumer/pcap.go
+++ consumer/pcap.go
@@ -17,73 +17,90 @@ package consumer
 import (
 	"bufio"
 	"bytes"
-	"errors"
 	"fmt"
 	"github.com/miekg/pcap"
 	"github.com/trivago/gollum/core"
+	"github.com/trivago/gollum/core/log"
 	"github.com/trivago/gollum/shared"
 	"net/http"
 	"sort"
 	"strconv"
 	"strings"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
-// Pcap consumer plugin
-// Configuration example
-//
-//   - "consumer.Pcap":
-//     Enable: true
-//     Intf: eth0
-//     Port: 80
-//
-type Stream struct {
+type stream struct {
 	Key     string
 	Timer   *time.Timer
-	Packets StreamPackets
+	Packets streamPackets
 	Buf     bytes.Buffer
 }
 
+type streamPackets []*pcap.Packet
+
 const streamTimeOut = 3 * time.Second
 
-func getKey(p *pcap.Packet) (string, error) {
-	if len(p.Headers) == 2 {
-		h0 := p.Headers[0].(*pcap.Iphdr)
-		h1 := p.Headers[1].(*pcap.Tcphdr)
-		s := fmt.Sprintf("%s:%d-%s:%d", h0.SrcAddr(), h1.SrcPort, h0.DestAddr(), h1.DestPort)
-		return s, nil
+func (a streamPackets) Len() int           { return len(a) }
+func (a streamPackets) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a streamPackets) Less(i, j int) bool { return seqNum(a[i]) < seqNum(a[j]) }
+
+func getKey(pkt *pcap.Packet) (string, error) {
+	if len(pkt.Headers) == 2 {
+		ipHeader := pkt.Headers[0].(*pcap.Iphdr)
+		tcpHeader := pkt.Headers[1].(*pcap.Tcphdr)
+		key := fmt.Sprintf("%s:%d-%s:%d", ipHeader.SrcAddr(), tcpHeader.SrcPort, ipHeader.DestAddr(), tcpHeader.DestPort)
+		return key, nil
 	}
-	return "", errors.New("Invalid Packet")
+	return "", fmt.Errorf("Invalid Packet")
 }
 
-func (s *Stream) reqsFromBuffer(b *bytes.Buffer) (rs []*bytes.Buffer, err error) {
-	r := bufio.NewReader(b)
-	for !readerDone(r) {
-		var req *http.Request
-		req, err = http.ReadRequest(r)
-		if err == nil {
-			req.Header.Add("X-Timestamp", strconv.FormatInt(time.Now().Unix(), 10))
-			idx := strings.IndexRune(s.Key, ':')
-			req.Header.Add("X-Client-Ip", s.Key[:idx])
-			resBuf := bytes.NewBuffer(nil)
-			err = req.Write(resBuf)
-			if err != nil {
-				goto Error
-			}
-			rs = append(rs, resBuf)
-		} else {
-			goto Error
+func readerDone(reader *bufio.Reader) bool {
+	_, err := reader.Peek(1)
+	return err != nil
+}
+
+func newStream(key string, p *Pcap) *stream {
+	return &stream{
+		Key:   key,
+		Timer: time.AfterFunc(streamTimeOut, func() { delete(p.Openstreams, key) }),
+	}
+}
+
+func (s *stream) reqsFromBuffer(reqBuffer *bytes.Buffer) ([]*bytes.Buffer, error) {
+	var reqArray []*bytes.Buffer
+	reqRead := bufio.NewReader(reqBuffer)
+
+	for !readerDone(reqRead) {
+		req, err := http.ReadRequest(reqRead)
+		if err != nil {
+			Log.Error.Print("Pcap: ", err)
+			return nil, err
 		}
+
+		idx := strings.IndexRune(s.Key, ':')
+		req.Header.Add("X-Timestamp", strconv.FormatInt(time.Now().Unix(), 10))
+		req.Header.Add("X-Client-Ip", s.Key[:idx])
+
+		modReqBuffer := bytes.NewBuffer(nil)
+		if err := req.Write(modReqBuffer); err != nil {
+			Log.Error.Print("Pcap: ", err)
+			return nil, err
+		}
+
+		reqArray = append(reqArray, modReqBuffer)
 	}
-	return
-Error:
-	rs = nil
-	fmt.Println(err.Error())
-	return
+
+	return reqArray, nil
 }
 
-func (s *Stream) processPacket(p *Pcap, pkt *pcap.Packet) {
+func seqNum(p *pcap.Packet) uint32 {
+	tcpHeader := p.Headers[1].(*pcap.Tcphdr)
+	return tcpHeader.Seq
+}
+
+func (s *stream) processPacket(p *Pcap, pkt *pcap.Packet) {
 	// We only care about packets with content
 	if len(pkt.Payload) > 0 {
 		// DEBUG--->
@@ -132,35 +149,26 @@ func (s *Stream) processPacket(p *Pcap, pkt *pcap.Packet) {
 	}
 }
 
-func seqNum(p *pcap.Packet) uint32 {
-	h := p.Headers[1].(*pcap.Tcphdr)
-	return h.Seq
-}
-
-type StreamPackets []*pcap.Packet
-
-func (a StreamPackets) Len() int           { return len(a) }
-func (a StreamPackets) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a StreamPackets) Less(i, j int) bool { return seqNum(a[i]) < seqNum(a[j]) }
-
+// Pcap consumer plugin
+// Configuration example
+//
+//   - "consumer.Pcap":
+//     Enable: true
+//     Interface: eth0
+//     Filter: "dst port 80 and dst host 127.0.0.1"
+//
 type Pcap struct {
 	core.ConsumerBase
-	Intf        string
-	Port        int
+	Interface   string
+	Filter      string
 	Capturing   bool
 	Handle      *pcap.Pcap
-	OpenStreams map[string]*Stream
-	GSeq        uint64
+	Openstreams map[string]*stream
+	seqNum      uint64
 }
 
 func init() {
 	shared.RuntimeType.Register(Pcap{})
-}
-
-func (p *Pcap) Close() {
-	p.Capturing = false
-	p.Handle.Close()
-	p.WorkerDone()
 }
 
 func (p *Pcap) Configure(conf core.PluginConfig) error {
@@ -169,77 +177,69 @@ func (p *Pcap) Configure(conf core.PluginConfig) error {
 		return err
 	}
 
-	p.Intf = conf.GetString("Intf", "eth0")
-	p.Port = conf.GetInt("Port", 80)
+	p.Interface = conf.GetString("Interface", "eth0")
+	p.Filter = conf.GetString("Filter", "dst port 80 and dst host 127.0.0.1")
 	p.Capturing = true
-	p.OpenStreams = make(map[string]*Stream)
+	p.Openstreams = make(map[string]*stream)
 
-	// device, snaplen, promisc, read timeout ms
-	p.Handle, err = pcap.OpenLive(p.Intf, int32(1<<16), true, 500)
-	if p.Handle == nil {
-		return err
-	}
-	expr := fmt.Sprintf("dst port %d", p.Port)
-
-	err = p.Handle.SetFilter(expr)
-	if err != nil {
-		return err
-	}
 	return nil
-}
-
-// A small hack to check if there is more to be read
-func readerDone(r *bufio.Reader) bool {
-	_, e := r.Peek(1)
-	return e != nil
 }
 
 func (p *Pcap) handleReqs(rs []*bytes.Buffer) {
 	for _, r := range rs {
-		p.Enqueue(r.Bytes(), p.GSeq)
-		p.GSeq++
+		p.Enqueue(r.Bytes(), p.seqNum)
+		atomic.AddUint64(&p.seqNum, 1)
 	}
-}
-
-func (p *Pcap) closeStream(s *Stream) {
-	// Stream closing...
-	delete(p.OpenStreams, s.Key)
-}
-
-func (p *Pcap) newStream(k string, pkt *pcap.Packet) *Stream {
-	s := &Stream{}
-	s.Key = k
-	s.Timer = time.AfterFunc(streamTimeOut, func() { p.closeStream(s) })
-	s.Packets = make([]*pcap.Packet, 0)
-	return s
 }
 
 func (p *Pcap) readPackets() {
 	for pkt, r := p.Handle.NextEx(); p.Capturing && r >= 0; pkt, r = p.Handle.NextEx() {
 		if r == 0 {
-			// timeout, continue
-			continue
+			continue // timeout, continue
 		}
 		pkt.Decode()
-		if k, err := getKey(pkt); err == nil {
-			stream, ok := p.OpenStreams[k]
+		if key, err := getKey(pkt); err == nil {
+			stream, ok := p.Openstreams[key]
 			if !ok {
-				stream = p.newStream(k, pkt)
-				p.OpenStreams[k] = stream
+				stream = newStream(key, p)
+				p.Openstreams[key] = stream
 			} else {
 				// Reset timer
 				stream.Timer.Reset(streamTimeOut)
 			}
 			stream.processPacket(p, pkt)
 		} else {
-			fmt.Println(err.Error())
+			Log.Error.Print("Pcap: ", err)
 		}
 	}
+	p.Handle.Close()
+	p.WorkerDone()
+}
+
+func (p *Pcap) close() {
+	p.Capturing = false
 }
 
 func (p *Pcap) Consume(workers *sync.WaitGroup) {
+	// Start listening
+	// device, snaplen, promisc, read timeout ms
+	var err error
+	p.Handle, err = pcap.OpenLive(p.Interface, int32(1<<16), true, 500)
+	if err != nil {
+		Log.Error.Print("Pcap: ", err)
+		return
+	}
+
+	err = p.Handle.SetFilter(p.Filter)
+	if err != nil {
+		p.Handle.Close()
+		Log.Error.Print("Pcap: ", err)
+		return
+	}
+
 	p.AddMainWorker(workers)
 	go p.readPackets()
-	defer p.Close()
+	defer p.close()
+
 	p.DefaultControlLoop(nil)
 }
