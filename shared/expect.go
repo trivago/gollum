ae310d9491123805db6a5e10219df47bb4253982
Bidesh Thapaliya <bidesh.thapaliya@trivago.com>
2015-09-02 12:24:44 +0200

    Adds test to consumer, pluginregistry and a ExecuteWithTimeout function in expect

--- shared/expect.go
+++ shared/expect.go
@@ -20,6 +20,7 @@ import (
 	"runtime"
 	"strings"
 	"testing"
+	"time"
 )
 
 var expectBasePath string
@@ -549,4 +550,22 @@ func (e Expect) MapGeq(data interface{}, key interface{}, value interface{}) boo
 	}
 
 	return e.Geq(value, valVal.Interface())
+}
+
+// ExecuteWithTimeout waits for the given timeout for the routine to return. If
+// timed out it is an error.
+func (e Expect) ExecuteWithTimeout(t time.Duration, routine func()) bool {
+	var cmd chan struct{}
+	go func() {
+		routine()
+		close(cmd)
+	}()
+
+	select {
+	case <-cmd:
+		return true
+	case <-time.After(t):
+		e.errorf("Evaluation timed out.")
+		return false
+	}
 }
