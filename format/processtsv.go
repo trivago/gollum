6c8e2261917b0380f36f37ee82df012443d1e87e
Daniel Thorn <daniel@relud.com>
2016-08-15 08:06:49 -0700

    add missing documentation

--- format/processtsv.go
+++ format/processtsv.go
@@ -37,7 +37,10 @@ import (
 //    ProcessTSVDirectives:
 //      - "0:time:20060102150405:2006-01-02 15\\:04\\:05"
 //      - "3:replace:°:\n"
+//      - "6:prefix:0."
+//      - "6:postfix:000"
 //      - "7:trim: "
+//      - "10:quote"
 //      - "11:remove"
 //      - "11:agent:browser:os:version"
 //
@@ -49,11 +52,14 @@ import (
 // The directives have to be given in the form of key:operation:parameters, where
 // operation can be one of the following:
 // - replace:<old>:<new> replace a given string in the value with a new one
+// - prefix:<string> add a given string to the start of the value
+// - postfix:<string> add a given string to the end of the value
 // - trim:<characters> remove the given characters (not string!) from the start
 // and end of the value
+// - quote add a " to the start and end of the value after processing
 // - timestamp:<read>:<write> read a timestamp and transform it into another
 // format
-// - remove removes the value
+// - remove remove the value
 // - agent:{<user_agent_field>:<user_agent_field>:...} Parse the value as a user
 // agent string and extract the given fields into <key>_<user_agent_field>
 // ("ua:agent:browser:os" would create the new fields "ua_browser" and "ua_os")
