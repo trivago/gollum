6317f52d4269c969b39761f8ab53396b078d0693
Arne Claus <arne.claus@trivago.com>
2016-09-05 16:44:24 +0200

    Added named pipe support for consumer.Console

--- consumer/console.go
+++ consumer/console.go
@@ -20,7 +20,10 @@ import (
 	"github.com/trivago/gollum/shared"
 	"io"
 	"os"
+	"strconv"
+	"strings"
 	"sync"
+	"time"
 )
 
 const (
@@ -34,13 +37,25 @@ const (
 // Configuration example
 //
 //  - "consumer.Console":
+//    Console: "stdin"
+//    Permissions: "0664"
 //    ExitOnEOF: false
+//
+// Console defines the pipe to read from. This can be "stdin" or the name
+// of a named pipe that is created if not existing. The default is "stdin"
+//
+// Permissions accepts an octal number string that contains the unix file
+// permissions used when creating a named pipe.
+// By default this is set to "0664".
 //
 // ExitOnEOF can be set to true to trigger an exit signal if StdIn is closed
 // (e.g. when a pipe is closed). This is set to false by default.
 type Console struct {
 	core.ConsumerBase
 	autoexit bool
+	pipe     *os.File
+	pipeName string
+	pipePerm uint32
 }
 
 func init() {
@@ -50,14 +65,41 @@ func init() {
 // Configure initializes this consumer with values from a plugin config.
 func (cons *Console) Configure(conf core.PluginConfig) error {
 	cons.autoexit = conf.GetBool("ExitOnEOF", false)
+	inputConsole := conf.GetString("Console", "stdin")
+
+	switch strings.ToLower(inputConsole) {
+	case "stdin":
+		cons.pipe = os.Stdin
+		cons.pipeName = "stdin"
+	default:
+		cons.pipe = nil
+		cons.pipeName = inputConsole
+
+		if perm, err := strconv.ParseInt(conf.GetString("Permissions", "0664"), 8, 32); err != nil {
+			Log.Error.Printf("Error parsing named pipe permissions: %s", err)
+		} else {
+			cons.pipePerm = uint32(perm)
+		}
+	}
+
 	return cons.ConsumerBase.Configure(conf)
 }
 
-func (cons *Console) readStdIn() {
-	buffer := shared.NewBufferedReader(consoleBufferGrowSize, 0, 0, "\n")
+func (cons *Console) readPipe() {
+	if cons.pipe == nil {
+		var err error
+		if cons.pipe, err = shared.OpenNamedPipe(cons.pipeName, cons.pipePerm); err != nil {
+			Log.Error.Print(err)
+			time.AfterFunc(3*time.Second, cons.readPipe)
+			return // ### return, try again ###
+		}
 
+		defer cons.pipe.Close()
+	}
+
+	buffer := shared.NewBufferedReader(consoleBufferGrowSize, 0, 0, "\n")
 	for cons.IsActive() {
-		err := buffer.ReadAll(os.Stdin, cons.Enqueue)
+		err := buffer.ReadAll(cons.pipe, cons.Enqueue)
 		cons.WaitOnFuse()
 		switch err {
 		case io.EOF:
@@ -71,13 +113,13 @@ func (cons *Console) readStdIn() {
 		case nil:
 			// ignore
 		default:
-			Log.Error.Print("Error reading stdin: ", err)
+			Log.Error.Print("Error reading pipe: ", err)
 		}
 	}
 }
 
 // Consume listens to stdin.
 func (cons *Console) Consume(workers *sync.WaitGroup) {
-	go cons.readStdIn()
+	go cons.readPipe()
 	cons.ControlLoop()
 }
