4c419af5e7c357d3fd8fadec0294d7b1053fef6b
Arne Claus <arne.claus@trivago.com>
2016-09-20 14:35:18 +0200

    Added go-systemd to gvt vendor file

--- contrib/native/systemdconsumer.go
+++ contrib/native/systemdconsumer.go
@@ -1,4 +1,3 @@
-//+build linux
 // Copyright 2015-2016 mozilla
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+// +build linux
 
 package native
 
