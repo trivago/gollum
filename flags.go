7d227137a0ef0bcdc34cd8f5af8599645e748567
Arne Claus <arne.claus@trivago.com>
2017-03-16 12:29:43 +0100

    Removed dependency hell for docker mflag package

--- flags.go
+++ flags.go
@@ -15,40 +15,30 @@
 package main
 
 import (
-	"fmt"
-	flag "gopkg.in/docker/docker.v1/pkg/mflag"
-	"os"
+	"github.com/trivago/tgo/tflag"
 )
 
 var (
-	flagHelp           = flag.Bool([]string{"h", "-help"}, false, "Print this help message.")
-	flagVersion        = flag.Bool([]string{"v", "-version"}, false, "Print version information and quit.")
-	flagReport         = flag.Bool([]string{"r", "-report"}, false, "Print detailed version report and quit.")
-	flagProfile        = flag.Bool([]string{"ps", "-profilespeed"}, false, "Write msg/sec measurements to log.")
-	flagLoglevel       = flag.Int([]string{"ll", "-loglevel"}, 0, "Set the loglevel [0-3]. Higher levels produce more messages.")
-	flagNumCPU         = flag.Int([]string{"n", "-numcpu"}, 0, "Number of CPUs to use. Set 0 for all CPUs.")
-	flagMetricsPort    = flag.Int([]string{"m", "-metrics"}, 0, "Port to use for metric queries. Set 0 to disable.")
-	flagConfigFile     = flag.String([]string{"c", "-config"}, "", "Use a given configuration file.")
-	flagTestConfigFile = flag.String([]string{"tc", "-testconfig"}, "", "Test a given configuration file and exit.")
-	flagCPUProfile     = flag.String([]string{"pc", "-profilecpu"}, "", "Write CPU profiler results to a given file.")
-	flagMemProfile     = flag.String([]string{"pm", "-profilemem"}, "", "Write heap profile results to a given file.")
-	flagTrace          = flag.String([]string{"tr", "-trace"}, "", "Write trace results to a given file.")
-	flagPidFile        = flag.String([]string{"p", "-pidfile"}, "", "Write the process id into a given file.")
+	flagHelp           = tflag.Switch("h", "help", "Print this help message.")
+	flagVersion        = tflag.Switch("v", "version", "Print version information and quit.")
+	flagReport         = tflag.Switch("r", "report", "Print detailed version report and quit.")
+	flagModules        = tflag.Switch("l", "list", "Print plugin information and quit.")
+	flagConfigFile     = tflag.String("c", "config", "", "Use a given configuration file.")
+	flagTestConfigFile = tflag.String("tc", "testconfig", "", "Test the given configuration file and exit.")
+	flagLoglevel       = tflag.Int("ll", "loglevel", 1, "Set the loglevel [0-3] as in {0=Errors, 1=+Warnings, 2=+Notes, 3=+Debug}.")
+	flagNumCPU         = tflag.Int("n", "numcpu", 0, "Number of CPUs to use. Set 0 for all CPUs.")
+	flagPidFile        = tflag.String("p", "pidfile", "", "Write the process id into a given file.")
+	flagMetricsPort    = tflag.Int("m", "metrics", 0, "Address to use for metric queries. Disabled by default.")
+	flagCPUProfile     = tflag.String("pc", "profilecpu", "", "Write CPU profiler results to a given file.")
+	flagMemProfile     = tflag.String("pm", "profilemem", "", "Write heap profile results to a given file.")
+	flagProfile        = tflag.Switch("ps", "profilespeed", "Write msg/sec measurements to log.")
+	flagTrace          = tflag.String("tr", "trace", "", "Write trace results to a given file.")
 )
 
-func init() {
-	flag.Usage = func() {
-		fmt.Println("Usage: gollum [OPTIONS]\n\nGollum - A n:m message multiplexer.\n\nOptions:")
-		flag.CommandLine.SetOutput(os.Stdout)
-		flag.PrintDefaults()
-		fmt.Print("\n")
-	}
-}
-
 func parseFlags() {
-	flag.Parse()
+	tflag.Parse()
 }
 
 func printFlags() {
-	flag.Usage()
+	tflag.PrintFlags("Usage: gollum [OPTIONS]\n\nGollum - A n:m message multiplexer.\n\nOptions:")
 }
