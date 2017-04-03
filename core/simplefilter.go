10b3dda14ab8c7f87698cb679fe086b5c9127862
msiebeneicher <marc.siebeneicher@trivago.com>
2017-04-03 12:14:41 +0200

    Fix drop stream id

--- core/simplefilter.go
+++ core/simplefilter.go
@@ -44,7 +44,7 @@ func (filter *SimpleFilter) SetLogScope(log tlog.LogScope) {
 func (filter *SimpleFilter) Configure(conf PluginConfigReader) error {
 	filter.Log = conf.GetSubLogScope("Filter")
 
-	filter.dropStreamID = GetStreamID(conf.GetString("DropToStream", InvalidStream))
+	filter.dropStreamID = GetStreamID(conf.GetString("DropToStream", DroppedStream))
 	return nil
 }
 
