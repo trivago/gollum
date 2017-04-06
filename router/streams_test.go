893cd49e7fe1df25b4fbf32df50e0f1724e1fd94
msiebeneicher <marc.siebeneicher@trivago.com>
2017-04-06 13:48:01 +0200

    Ranamed package

--- router/streams_test.go
+++ router/streams_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package stream
+package router
 
 import (
 	"github.com/trivago/gollum/core"
