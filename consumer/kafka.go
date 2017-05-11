8f8e62bd8cb8854ae4a69666b07c1718873167ad
msiebeneicher <marc.siebeneicher@trivago.com>
2017-05-11 10:24:00 +0200

    Removed deprecated `PrependKey` and `KeySeparator` handling

--- consumer/kafka.go
+++ consumer/kafka.go
@@ -75,8 +75,6 @@ const (
 //    	SaslEnabled: false
 //    	SaslUsername: "gollum"
 //    	SaslPassword: ""
-//    	PrependKey: false #deprecated => use meta data `key`
-//    	KeySeparator: ":"
 //    	Servers:
 //        - "localhost:9092"
 //
@@ -109,13 +107,6 @@ const (
 // Ordered can be set to enforce partitions to be read one-by-one in a round robin
 // fashion instead of reading in parallel from all partitions.
 // Set to false by default. Ignored when using GroupId.
-//
-// PrependKey can be enabled to prefix the read message with the key from the
-// kafka message. A separator will ba appended to the key. See KeySeparator.
-// By default this is option set to false.
-//
-// KeySeparator defines the separator that is appended to the kafka message key
-// if PrependKey is set to true. Set to ":" by default.
 //
 // MaxOpenRequests defines the number of simultaneous connections are allowed.
 // By default this is set to 5.
@@ -194,8 +185,6 @@ type Kafka struct {
 	MaxPartitionID    int32
 	persistTimeout    time.Duration
 	orderedRead       bool
-	prependKey        bool
-	keySeparator      []byte
 	folderPermissions os.FileMode
 }
 
@@ -215,12 +204,6 @@ func (cons *Kafka) Configure(conf core.PluginConfigReader) error {
 	cons.orderedRead = conf.GetBool("Ordered", false)
 	cons.offsets = make(map[int32]*int64)
 	cons.MaxPartitionID = 0
-	cons.keySeparator = []byte(conf.GetString("KeySeparator", ":"))
-
-	if conf.HasValue("PrependKey") {
-		cons.Log.Warning.Print("Deprecated config value 'PrependKey' found. Update to meta data usage 'key' in the near future.")
-	}
-	cons.prependKey = conf.GetBool("PrependKey", false)
 
 	folderFlags, err := strconv.ParseInt(conf.GetString("FolderPermissions", "0755"), 8, 32)
 	cons.folderPermissions = os.FileMode(folderFlags)
@@ -379,14 +362,6 @@ func (cons *Kafka) restartGroup() {
 	cons.readFromGroup()
 }
 
-func (cons *Kafka) keyedMessage(key []byte, value []byte) []byte {
-	buffer := make([]byte, len(key)+len(cons.keySeparator)+len(value))
-	offset := copy(buffer, key)
-	offset += copy(buffer[offset:], cons.keySeparator)
-	copy(buffer[offset:], value)
-	return buffer
-}
-
 // Main fetch loop for kafka events
 func (cons *Kafka) readFromGroup() {
 	consumer, err := cluster.NewConsumerFromClient(cons.groupClient, cons.group, []string{cons.topic})
@@ -536,11 +511,7 @@ func (cons *Kafka) enqueueEvent(event *kafka.ConsumerMessage) {
 	metaData.SetValue("topic", []byte(event.Topic))
 	metaData.SetValue("key", event.Key)
 
-	if cons.prependKey {
-		cons.EnqueueWithMetaData(cons.keyedMessage(event.Key, event.Value), metaData)
-	} else {
-		cons.EnqueueWithMetaData(event.Value, metaData)
-	}
+	cons.EnqueueWithMetaData(event.Value, metaData)
 }
 
 func (cons *Kafka) startReadTopic(topic string) {
