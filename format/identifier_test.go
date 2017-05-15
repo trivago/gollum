d0c79a7fd778f017dc59549f4b072fb8aac3191a
msiebeneicher <marc.siebeneicher@trivago.com>
2017-05-15 17:21:29 +0200

    added `applyTo` handling to Identifier formatter

--- format/identifier_test.go
+++ format/identifier_test.go
@@ -24,3 +24,24 @@ func TestFormatterIdentifier(t *testing.T) {
 
 	expect.Equal("f9e6e6ef197c2b25", string(msg.Data()))
 }
+
+func TestFormatterIdentifierApplyTo(t *testing.T) {
+	expect := ttesting.NewExpect(t)
+
+	config := core.NewPluginConfig("", "format.Identifier")
+	config.Override("ApplyTo", "foo")
+	config.Override("Use", "hash")
+	plugin, err := core.NewPluginWithConfig(config)
+	expect.NoError(err)
+
+	formatter, casted := plugin.(*Identifier)
+	expect.True(casted)
+
+	msg := core.NewMessage(nil, []byte("payload"), 0, core.InvalidStreamID)
+	msg.MetaData().SetValue("foo", []byte("test"))
+	err = formatter.ApplyFormatter(msg)
+	expect.NoError(err)
+
+	expect.Equal("f9e6e6ef197c2b25", msg.MetaData().GetValueString("foo"))
+	expect.Equal("payload", msg.String())
+}
