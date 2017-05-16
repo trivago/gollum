89d9509502b8a807ab3b87755a57be3f2682ffbd
msiebeneicher <marc.siebeneicher@trivago.com>
2017-05-16 10:36:14 +0200

    added `applyTo` handling to RegExp formatter

--- format/regexp_test.go
+++ format/regexp_test.go
@@ -28,3 +28,28 @@ func TestFormatterRegExp(t *testing.T) {
 
 	expect.Equal("test", string(msg.Data()))
 }
+
+func TestFormatterRegExpApplyTo(t *testing.T) {
+	expect := ttesting.NewExpect(t)
+
+	config := core.NewPluginConfig("", "format.RegExp")
+	config.Override("Expression", "([a-z]*)")
+	config.Override("Template", "${1}")
+	config.Override("ApplyTo", "foo")
+
+	plugin, err := core.NewPluginWithConfig(config)
+	expect.NoError(err)
+
+	formatter, casted := plugin.(*RegExp)
+	expect.True(casted)
+
+	msg := core.NewMessage(nil, []byte("PAYLOAD"),
+		0, core.InvalidStreamID)
+	msg.MetaData().SetValue("foo", []byte("test 123"))
+
+	err = formatter.ApplyFormatter(msg)
+	expect.NoError(err)
+
+	expect.Equal("PAYLOAD", string(msg.Data()))
+	expect.Equal("test", msg.MetaData().GetValueString("foo"))
+}
