ac1f23a3db538632a0f9d411cabdcbcc471fda2b
msiebeneicher <marc.siebeneicher@trivago.com>
2017-05-16 14:07:35 +0200

    added `applyTo` handling to StreamName formatter

--- format/streamname_test.go
+++ format/streamname_test.go
@@ -60,3 +60,48 @@ func TestStreamNameHistory(t *testing.T) {
 
 	expect.Equal(core.LogInternalStream+":test", msg.String())
 }
+
+func TestStreamNameApplyTo(t *testing.T) {
+	expect := ttesting.NewExpect(t)
+
+	config := core.NewPluginConfig("", "format.StreamName")
+	config.Override("ApplyTo", "foo")
+
+	plugin, err := core.NewPluginWithConfig(config)
+	expect.NoError(err)
+
+	formatter, casted := plugin.(*StreamName)
+	expect.True(casted)
+
+	msg := core.NewMessage(nil, []byte("payload"), core.LogInternalStreamID)
+	msg.MetaData().SetValue("foo", []byte("test"))
+
+	err = formatter.ApplyFormatter(msg)
+	expect.NoError(err)
+
+	expect.Equal("payload", msg.String())
+	expect.Equal(core.LogInternalStream+":test", msg.MetaData().GetValueString("foo"))
+}
+
+func TestStreamNameApplyToNoSeparator(t *testing.T) {
+	expect := ttesting.NewExpect(t)
+
+	config := core.NewPluginConfig("", "format.StreamName")
+	config.Override("ApplyTo", "foo")
+	config.Override("Separator", "")
+
+	plugin, err := core.NewPluginWithConfig(config)
+	expect.NoError(err)
+
+	formatter, casted := plugin.(*StreamName)
+	expect.True(casted)
+
+	msg := core.NewMessage(nil, []byte("payload"), core.LogInternalStreamID)
+	msg.MetaData().SetValue("foo", []byte("test"))
+
+	err = formatter.ApplyFormatter(msg)
+	expect.NoError(err)
+
+	expect.Equal("payload", msg.String())
+	expect.Equal(core.LogInternalStream, msg.MetaData().GetValueString("foo"))
+}
