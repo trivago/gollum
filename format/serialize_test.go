b012028466dbdd1935917f9341653689021e94bc
msiebeneicher <marc.siebeneicher@trivago.com>
2017-05-16 11:09:27 +0200

    added `applyTo` handling to Serialize formatter

--- format/serialize_test.go
+++ format/serialize_test.go
@@ -29,3 +29,28 @@ func TestFormatterSerialize(t *testing.T) {
 
 	expect.Equal("foo bar", string(controlMsg.Data()))
 }
+
+func TestFormatterSerializeApplyTo(t *testing.T) {
+	expect := ttesting.NewExpect(t)
+
+	config := core.NewPluginConfig("", "format.Serialize")
+	config.Override("ApplyTo", "foo")
+
+	plugin, err := core.NewPluginWithConfig(config)
+	expect.NoError(err)
+
+	formatter, casted := plugin.(*Serialize)
+	expect.True(casted)
+
+	msg := core.NewMessage(nil, []byte("foo bar"),
+		0, core.InvalidStreamID)
+
+	err = formatter.ApplyFormatter(msg)
+	expect.NoError(err)
+
+	controlMsg, err := core.DeserializeMessage(msg.MetaData().GetValue("foo"))
+	expect.NoError(err)
+
+	expect.Equal("foo bar", controlMsg.String())
+	expect.Equal("foo bar", msg.String())
+}
