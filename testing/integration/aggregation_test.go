dbbee34d11a60e82e3db563e30c829a3ae4ad0d5
msiebeneicher <marc.siebeneicher@trivago.com>
2017-05-17 16:48:43 +0200

    added integration test for aggregated pipeline

--- testing/integration/aggregation_test.go
+++ testing/integration/aggregation_test.go
@@ -6,10 +6,14 @@ import (
 	"github.com/trivago/tgo/ttesting"
 	"strings"
 	"testing"
+	"os"
+	"time"
+	"fmt"
 )
 
 const (
 	testProducerAggregationConfig = "test_aggregated_producers.conf"
+	testPipelineAggregationConfig = "test_aggregated_pipeline.conf"
 )
 
 func TestProducerAggregation(t *testing.T) {
@@ -35,4 +39,50 @@ func TestProducerAggregation(t *testing.T) {
 
 	expect.True(strings.Contains(resultFileProducer1.content, "123"))
 	expect.True(strings.Contains(resultFileProducer2.content, "123"))
+}
+
+func TestProducerAggregationPipeline(t *testing.T) {
+	setup()
+	expect := ttesting.NewExpect(t)
+
+	// create files
+	fileFoo, err := os.OpenFile(TmpTestFilePathFoo, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	defer fileFoo.Close()
+	expect.NoError(err)
+
+	_, err = fileFoo.Write([]byte("hello\ngo\n"))
+	expect.NoError(err)
+
+	fileBar, err := os.OpenFile(TmpTestFilePathBar, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	defer fileBar.Close()
+	expect.NoError(err)
+
+	_, err = fileBar.Write([]byte("hello\ngo\n"))
+	expect.NoError(err)
+
+	// execute gollum
+	cmd := ExecuteGollumAndGetCmd(testPipelineAggregationConfig, []string{}, "-ll=2")
+	time.Sleep(2 * time.Second) // wait till gollum should booted
+
+	// write more to files - result for gollum
+	_, err = fileFoo.Write([]byte("foo\n"))
+	expect.NoError(err)
+
+	_, err = fileBar.Write([]byte("bar\n"))
+	expect.NoError(err)
+
+	// wait until gollum process is done
+	cmd.Wait()
+
+	out := fmt.Sprint(cmd.Stdout)
+	expect.True(strings.Contains(out, "(startup)"))
+
+	// get results from file targets
+	ResultFile, err := GetResultFile(TmpTestFilePathDefault)
+	expect.NoError(err)
+
+	// final expectations filter in producer
+	expect.True(strings.Contains(ResultFile.content, "foo"))
+	expect.True(strings.Contains(ResultFile.content, "bar"))
+	expect.Equal(4, ResultFile.lines)
 }
