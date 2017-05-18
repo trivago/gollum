d78ca8f71292289419662e9e04617859a841d9f2
Arne Claus <arne.claus@trivago.com>
2017-05-18 15:01:48 +0200

    Cleanup of types in message.go

---
+++ core/messagesource.go
@@ -0,0 +1,56 @@
+// Copyright 2015-2017 trivago GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package core
+
+// MessageSource defines methods that are common to all message sources.
+// Currently this is only a placeholder.
+type MessageSource interface {
+	// IsActive returns true if the source can produce messages
+	IsActive() bool
+
+	// IsBlocked returns true if the source cannot produce messages
+	IsBlocked() bool
+}
+
+// AsyncMessageSource extends the MessageSource interface to allow a backchannel
+// that simply forwards any message coming from the producer.
+type AsyncMessageSource interface {
+	MessageSource
+
+	// EnqueueResponse sends a message to the source of another message.
+	EnqueueResponse(msg *Message)
+}
+
+// SerialMessageSource extends the AsyncMessageSource interface to allow waiting
+// for all parts of the response to be submitted.
+type SerialMessageSource interface {
+	AsyncMessageSource
+
+	// Notify the end of the response stream
+	ResponseDone()
+}
+
+// LinkableMessageSource extends the MessageSource interface to allow a pipe
+// like behaviour between two components that communicate messages.
+type LinkableMessageSource interface {
+	MessageSource
+	// Link the message source to the message receiver. This makes it possible
+	// to create stable "pipes" between e.g. a consumer and producer.
+	Link(pipe interface{})
+
+	// IsLinked has to return true if Link executed successful and does not
+	// need to be called again.
+	IsLinked() bool
+}
