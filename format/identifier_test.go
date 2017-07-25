99f6016c95a67cbb1f1affd5b7886654b9e021df
Arne Claus <arne.claus@trivago.com>
2017-07-25 14:44:21 +0200

    fix the format.identifier unittest

--- format/identifier_test.go
+++ format/identifier_test.go
@@ -11,7 +11,7 @@ func TestFormatterIdentifier(t *testing.T) {
 	expect := ttesting.NewExpect(t)
 
 	config := core.NewPluginConfig("", "format.Identifier")
-	config.Override("Use", "hash")
+	config.Override("Generator", "hash")
 	plugin, err := core.NewPluginWithConfig(config)
 	expect.NoError(err)
 
@@ -30,18 +30,18 @@ func TestFormatterIdentifierApplyTo(t *testing.T) {
 
 	config := core.NewPluginConfig("", "format.Identifier")
 	config.Override("ApplyTo", "foo")
-	config.Override("Use", "hash")
+	config.Override("Generator", "hash")
 	plugin, err := core.NewPluginWithConfig(config)
 	expect.NoError(err)
 
 	formatter, casted := plugin.(*Identifier)
 	expect.True(casted)
 
-	msg := core.NewMessage(nil, []byte("payload"), nil, core.InvalidStreamID)
-	msg.GetMetadata().SetValue("foo", []byte("test"))
+	msg := core.NewMessage(nil, []byte("test"), nil, core.InvalidStreamID)
+	msg.GetMetadata().SetValue("foo", []byte(""))
 	err = formatter.ApplyFormatter(msg)
 	expect.NoError(err)
 
 	expect.Equal("f9e6e6ef197c2b25", msg.GetMetadata().GetValueString("foo"))
-	expect.Equal("payload", msg.String())
+	expect.Equal("test", msg.String())
 }
