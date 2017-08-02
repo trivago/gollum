f488437cd5a6e1291379c6e743d9cfad20742761
Arne Claus <arne.claus@trivago.com>
2017-08-02 15:05:27 +0200

    add a new router working on metadata fields

---
+++ router/metadata.go
@@ -0,0 +1,69 @@
+// Copyright 2015-2017 trivago GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package router
+
+import (
+	"github.com/trivago/gollum/core"
+)
+
+// Metadata router
+//
+// This router routes the message to a stream given in a specified metadata
+// field. If the field is not set, the message will be passed along.
+//
+// Parameters
+//
+// - Key: The metadata field to read from.
+// By default this parameter is set to "Stream"
+//
+// Examples
+//
+//  switchRoute:
+//    Type: router.Metadata
+//    Stream: errorlogs
+//    Key: key
+type Metadata struct {
+	Broadcast `gollumdoc:"embed_type"`
+	key       string `config:"Key" default:"Stream"`
+}
+
+func init() {
+	core.TypeRegistry.Register(Metadata{})
+}
+
+// Configure initializes this distributor with values from a plugin config.
+func (router *Metadata) Configure(conf core.PluginConfigReader) {
+}
+
+// Start the router
+func (router *Metadata) Start() error {
+	return nil
+}
+
+// Enqueue enques a message to the router
+func (router *Metadata) Enqueue(msg *core.Message) error {
+	streamName := msg.GetMetadata().GetValueString(router.key)
+
+	if len(streamName) > 0 {
+		targetID := core.GetStreamID(string(streamName))
+		router := core.StreamRegistry.GetRouterOrFallback(targetID)
+
+		msg.SetStreamID(targetID)
+		core.Route(msg, router)
+		return nil
+	}
+
+	return router.Broadcast.Enqueue(msg)
+}
