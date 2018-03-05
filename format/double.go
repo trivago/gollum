f773cbd429cbda2ed7ea95e34b7dc361b2c2656a
Daniel Kleuser <daniel.kleuser@trivago.com>
2018-03-05 15:02:42 +0100

    Fixed years for copyright

--- format/double.go
+++ format/double.go
@@ -1,4 +1,4 @@
-// Copyright 2015 trivago N.V.
+// Copyright 2015-2018 trivago N.V.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
