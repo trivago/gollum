571f6846fc2ec83aa63a1f859bca2dc0ae4b5bde
Matthias Endler <matthias-endler@gmx.net>
2019-06-07 11:31:33 +0200

    Add permission support to syslog sockets (#254)

--- consumer/syslogd.go
+++ consumer/syslogd.go
@@ -52,6 +52,11 @@ import (
 // * RFC6587 (https://tools.ietf.org/html/rfc6587) - unix, upd, tcp
 // By default this parameter is set to "RFC6587".
 //
+// - Permissions: This value sets the filesystem permissions
+// as a four-digit octal number in case the address is a Unix domain socket
+// (i.e. unix://<filesystem-path>).
+// By default this parameter is set to "0770".
+//
 // - SetMetadata: When set to true, syslog based metadata will be attached to
 // the message. The metadata fields added depend on the protocol version used.
 // RFC3164 supports: tag, timestamp, hostname, priority, facility, severity.
@@ -86,8 +91,9 @@ type Syslogd struct {
 	format              format.Format // RFC3164, RFC5424 or RFC6587?
 	protocol            string
 	address             string
-	withMetadata        bool   `config:"SetMetadata" default:"false"`
-	timestampFormat     string `config:"TimestampFormat" default:"2006-01-02T15:04:05.000 MST"`
+	withMetadata        bool        `config:"SetMetadata" default:"false"`
+	fileFlags           os.FileMode `config:"Permissions" default:"0770"`
+	timestampFormat     string      `config:"TimestampFormat" default:"2006-01-02T15:04:05.000 MST"`
 }
 
 func init() {
@@ -211,9 +217,14 @@ func (cons *Syslogd) Consume(workers *sync.WaitGroup) {
 
 	switch cons.protocol {
 	case "unix":
+		err := os.Chmod(cons.address, cons.fileFlags)
+		if err != nil {
+			cons.Logger.WithError(err).Error("Failed to set file permissions on", cons.address)
+		}
+
 		if err := server.ListenUnixgram(cons.address); err != nil {
 			if errRemove := os.Remove(cons.address); errRemove != nil {
-				cons.Logger.WithError(errRemove).Error("Failed to remove exisiting socket")
+				cons.Logger.WithError(errRemove).Error("Failed to remove existing socket")
 			} else {
 				cons.Logger.Warning("Found existing socket ", cons.address, ". Removing.")
 				err = server.ListenUnixgram(cons.address)
