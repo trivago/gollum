0540b0d40e750b6904dd4d8352a084b72f7e889e
Arne Claus <arne.claus@trivago.com>
2019-07-30 13:49:03 +0200

    add an override formatter to set constant data

---
+++ format/override.go
@@ -0,0 +1,53 @@
+// Copyright 2015-2018 trivago N.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package format
+
+import (
+	"github.com/trivago/gollum/core"
+)
+
+// Override formatter
+//
+// This formatter sets a given value to a metadata field or payload.
+//
+// Examples
+//
+// This example sets the value "foo" on the key "bar".
+//
+//  exampleConsumer:
+//    Type: consumer.Console
+//    Streams: stdin
+//    Modulators:
+//      - format.Override
+//        Target: bar
+//        Value: "foo"
+type Override struct {
+	core.SimpleFormatter `gollumdoc:"embed_type"`
+	value                string `config:"Value"`
+}
+
+func init() {
+	core.TypeRegistry.Register(Override{})
+}
+
+// Configure initializes this formatter with values from a plugin config.
+func (format *Override) Configure(conf core.PluginConfigReader) {
+}
+
+// ApplyFormatter update message payload
+func (format *Override) ApplyFormatter(msg *core.Message) error {
+	format.SetTargetData(msg, format.value)
+	return nil
+}
